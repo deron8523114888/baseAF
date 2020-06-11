8d9b89c96e8b09916b1aba285667f3241a46932f
deron <deron8523114888@gmail.com>
2020-06-11 14:47:51 +0800

    修改 Activity to FragmentActivity

--- lib-ui-tocleanmvp/src/main/java/co.bxvip.ui.tocleanmvp.base/BaseActivity.java
+++ lib-ui-tocleanmvp/src/main/java/co.bxvip.ui.tocleanmvp.base/BaseActivity.java
@@ -10,6 +10,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
@@ -23,7 +24,7 @@ import android.view.WindowManager;
  * </pre>
  */
 
-public abstract class BaseActivity extends Activity implements IBaseAF {
+public abstract class BaseActivity extends FragmentActivity implements IBaseAF {
     protected String TAG = this.getClass().getName();
     protected Context mContext;
 
